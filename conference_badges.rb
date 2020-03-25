f5663caf0d809949e28656e908575976e99b0965
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 02:03:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array_names)
-  array_badge = []
+  newarray = []
   array_names.each do |name|
     newarray.push("Hello, my name is #{name}.")
   end

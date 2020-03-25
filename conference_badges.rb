a6b04244ca15ffeaf1cf7aa92f50a9aecdcc3bf4
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 01:41:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
 
 
-##def batch_badge_creator(array_names)
-  #array_names = []
-  #array_names
-#end 
+def batch_badge_creator(array_names)
+  array_names = []
+  array_names
+end 

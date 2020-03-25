0a9d2bcbdb55836d8841b7f7ef85d0b052279db3
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 01:38:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,8 @@
 # Write your code here.
 def badge_maker(name)
-  badges = []
-  counter = 0
-  while counter < badges.length-1
-  badges << name
-  return "Hello, my name is " + badges[counter] + " ."
-  end
-end 
+ return "Hello, my name is #{name}"
+end
+
 
 ##def batch_badge_creator(array_names)
   #array_names = []

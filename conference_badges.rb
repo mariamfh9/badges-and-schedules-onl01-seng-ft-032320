fbeb2f01d7b1bbc4c918dec22e9abd741b7f9d95
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 02:15:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,11 +23,11 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(array).each do |id|
-    puts id
+  batch_badge_creator(array).each do |list|
+    puts list
   end 
   
-  assign_rooms(array).each do |id|
-    puts id 
+  assign_rooms(array).each do |list|
+    puts list 
   end
 end 

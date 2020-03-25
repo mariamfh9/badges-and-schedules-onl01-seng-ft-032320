490fc973dda594de9f62b50e20af231bac79c438
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 02:24:23 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_names)
+  newarray = []
+  array_names.each do |name|
+    newarray.push("Hello, my name is #{name}.")
+  end
+  return newarray
+end
+
+
+def assign_rooms(array)
+  newarray = []
+  counter = 1
+  array.each do |name|
+    newarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return newarray
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |list|
+    puts list
+  end 
+  
+  assign_rooms(array).each do |list|
+    puts list 
+  end
+end 
